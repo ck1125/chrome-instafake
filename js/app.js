10b7f71220fce9af061ec4cf0b6e08893cabf7da
Eli Ezeugoh <cezeugoh@gmail.com>
2012-11-19 17:00:05 +0000

    added other effects.

--- js/app.js
+++ js/app.js
@@ -51,6 +51,7 @@ var chooseFileButton = document.querySelector('#choose_file');
 var saveFileButton = document.querySelector('#save_file');
 var output = document.querySelector('output');
 var img = document.querySelector('img');
+var untransformedImage;
 
 
 chooseFileButton.addEventListener('click', function(e) {
@@ -68,6 +69,7 @@ chooseFileButton.addEventListener('click', function(e) {
     chosenFileEntry.file(function(file) {
       readFileEntry(readOnlyEntry, function(result) {
         img.src = result;
+		untransformedImage = result;
       });
       // Update display.
       writeFileButton.disabled = false;
@@ -107,6 +109,7 @@ var dnd = new DnDFileController('body', function(data) {
   chosenFileEntry = item.webkitGetAsEntry();
   readFileEntry(chosenFileEntry, function(result) {
     img.src = result;
+    untransformedImage = result;
 
   });
   // Update display.
--- js/vintageEffects.js
+++ js/vintageEffects.js
@@ -1,22 +1,57 @@
-var applied = false;
-var VIGNETTE_BORDER = {
-	vignette: {black:0,white:0},
-	noise: false,
-	screen: false,
-	desaturate: false,
-	allowMultiEffect: true,
-	viewFinder: 'images/viewfinder_bw.jpg',
-	curves: false,
-	blur: false
+var imageTransforms = { 
+	vignette : {
+		vignette: {black:0,white:0},
+		noise: false,
+		screen: false,
+		desaturate: false,
+		allowMultiEffect: true,
+		viewFinder: 'images/viewfinder_bw.jpg',
+		curves: false,
+		blur: false
+	},
+	sepia : {
+        vignette: {
+            black: 0.6,
+            white: 0.1
+        },
+        noise: 25,
+        screen: {
+            red: 141,
+            green: 107,
+            blue: 3,
+            strength: 0.47
+        },
+        desaturate: 0.7,
+        allowMultiEffect: true,
+		viewFinder: 'images/viewfinder_bw.jpg',
+        mime: 'image/jpeg',
+        viewFinder: false
+    },
+    grayscale : {
+        vignette: {
+            black: 0.7,
+            white: 0.2
+        },
+        noise: 25,
+        screen: false,
+        desaturate: 1,
+        allowMultiEffect: true,
+        mime: 'image/jpeg',
+		viewFinder: 'images/viewfinder_bw.jpg',
+        viewFinder: false
+    }	
+			
 };
 
 $(document).ready(function() {		
-		$('.vintage').click(function () {
-			var imageSrc = $('.vintage').attr('src');
-			if (!applied && imageSrc) {
-				console.log('coded!');
-	        	$(this).vintage(VIGNETTE_BORDER);
-				applied = true;
+		$('.chopped.box').click(function () {
+			var imageSrc = $('img.vintage').attr('src');
+			if (imageSrc) {
+				console.log('image loaded!');
+				var transform = $(this).data('transform');
+				console.log('selected transform ->'+transform);
+				console.log('transform config ->'+JSON.stringify(imageTransforms[transform]));
+	        	$('img.vintage').vintage(imageTransforms[transform]);
 			}	
 		});
 		
